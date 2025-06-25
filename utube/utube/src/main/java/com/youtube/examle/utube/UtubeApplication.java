b68d4b2a8631bb16f8328e39d817d4b54fb5c727
Abhishek Gupta <abhishekguptasde@gmail.com>
2025-06-25 17:22:25 +0530

    init commit

---
+++ utube/utube/src/main/java/com/youtube/examle/utube/UtubeApplication.java
@@ -0,0 +1,13 @@
+package com.youtube.examle.utube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtubeApplication.class, args);
+	}
+
+}
---
+++ utube/utube/src/test/java/com/youtube/examle/utube/UtubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youtube.examle.utube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
